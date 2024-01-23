6e12361fec68214536331ce964fc3b36b4cd3c06
Rachit Patel <rachitp509@gmail.com>
2024-01-23 11:13:59 +0530

    added baseUrl

--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000",
-  baseURL: "https://car-backend.adaptable.app",
+  // baseURL: "https://car-backend.adaptable.app",
+  baseURL: "https://api.asisauctions.com.au",
 });
 
 export default axiosInstance;
